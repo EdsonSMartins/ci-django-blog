5fc0d5add7cd5bd2979d1d57d597fa8a2abc8a29
EdsonSMartins <smartins.edson@gmail.com>
2024-02-27 22:32:24 +0000

    adds a post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-edsonsmarti-cidjangoblo-vl731o3w4jx.ws-eu108.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-edsonsmarti-cidjangoblo-ozwmwpl9bx4.ws-eu108.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -88,6 +88,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
